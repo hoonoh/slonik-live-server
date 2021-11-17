a18d72cd1e244bcc4c56189b3e0579b4faa3420b
hoonoh <hoonoh@users.noreply.github.com>
2021-11-17 18:37:49 +0900

    chore: remove build:prod, refactor util files

--- packages/ts-slonik-live-server-plugin/src/util/post-publish.ts
+++
@@ -1,5 +0,0 @@
-import { renameSync } from 'fs';
-import { resolve } from 'path';
-
-const packageJsonPath = resolve(__dirname, '../../package.json');
-renameSync(packageJsonPath + '.backup', packageJsonPath);
